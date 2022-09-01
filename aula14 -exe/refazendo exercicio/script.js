54150fda2eb24d6cb8e214b1773d9bea6e8a0c23
Cristiano83dorneles <109388189+Cristiano83dorneles@users.noreply.github.com>
2022-09-01 09:21:34 +0100

    treinamento ex tabuada

---
+++ aula14 -exe/refazendo exercicio/script.js
@@ -0,0 +1,8 @@
+function gerar() {
+    var inum = document.getElementById('inum')
+    var itab = document.getElementById('itab')
+    
+    if (inum.value.lenght == "") {
+        window.alert('[ERRO] Digite um valor')
+    }
+}
